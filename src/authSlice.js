34b7db05e6aef3eee661929b35d50ffa802254f1
Manjeetsaini25 <manjeetsaini2003.com@gmail.com>
2026-07-23 11:19:34 +0530

    add Redux state management for user registration - 07

---
+++ src/authSlice.js
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axiosClient from './utils/axiosClient'
+
+export const registerUser = createAsyncThunk(
+  'auth/register',
+  async (userData, { rejectWithValue }) => {
+    try {
+    const response =  await axiosClient.post('/user/register', userData);
+    return response.data.user;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    user: null,
+    isAuthenticated: false,
+    loading: false,
+    error: null
+  },
+  reducers: {
+  },
+  extraReducers: (builder) => {
+    builder
+      // Register User Cases
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.isAuthenticated = !!action.payload;
+        state.user = action.payload;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload?.message || 'Something went wrong';
+        state.isAuthenticated = false;
+        state.user = null;
+      })
+  }
+});
+
+export default authSlice.reducer;
